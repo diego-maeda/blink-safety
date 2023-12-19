28960d8c197d0bc0cd8b6501d2e27b52f699f1d0
diego.maeda <diego.maeda@solluti.com.br>
2023-12-19 18:00:00 -0300

    Updating the vite configs.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: true
         }),
-        basicSsl(),
     ],
 });
