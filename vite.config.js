9309f06e6e1e765c205c500f54b2039a81e62cb0
diego.maeda <diego.maeda@solluti.com.br>
2023-12-19 16:46:12 -0300

    Updating with Basic ssl on Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        basicSsl(),
     ],
 });
