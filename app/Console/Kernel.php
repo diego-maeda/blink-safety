f9a09bec03d585fc16aa1600d75606efd467af23
diego.maeda <diego.maeda@solluti.com.br>
2023-12-31 15:24:37 -0300

    Updating the command Kernel for typeO

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:retrieve-st-petersburg-data ')->everyFiveMinutes();
+        $schedule->command('app:retrieve-st-petersburg-data')->everyFiveMinutes();
     }
 
     /**
